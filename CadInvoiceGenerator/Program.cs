55247ea81ac89c189fc48619a84b593636c06da0
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-09-04 15:29:06 -0600

    Welcome message

---
+++ CadInvoiceGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CadInvoiceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to cab invoice generator");
+        }
+    }
+}
