a6fd3dc4313326ea6512571daf12c0b13e6dd6d9
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-09-04 15:54:04 -0600

    Feat: Calculated Multiple Rides

---
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -0,0 +1,41 @@
+using CadInvoiceGenerator;
+using NUnit.Framework;
+
+namespace CabInvoiceGeneratorTest
+{
+    public class Tests
+    {
+        InvoiceGenerator invoiceGenerator = null;
+
+        [Test]
+        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        {
+            //Creating Instance of Invoice Generator  For Normal Ride
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+
+            //Calculating Fare
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+
+            //Asserting Values
+            Assert.AreEqual(expected, fare);
+        }
+
+        [Test]
+        public void GivenMultipleRidesShouldReturnInvoiceSummary()
+        {
+            //Creating Instance of Invoice Generator  For Normal Ride
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+            //Generating Summary for Rides
+            InvoiceSummary invoiceSummary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
+
+            //Asserting Values
+            Assert.AreEqual(expectedSummary, invoiceSummary);
+        }
+    }
+}
